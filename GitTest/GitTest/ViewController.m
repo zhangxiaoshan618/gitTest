45c5cea658ae106074377b6823aba5cf69b54cd1
lixingyao <lixingyao001@lianjia.com>
2019-10-07 14:13:27 +0800

    设置UI

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -14,9 +14,23 @@
 
 @implementation ViewController
 
+- (void)loadView {
+    [super loadView];
+    self.view.backgroundColor = UIColor.clearColor;
+    [self setUpUI];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (void)loadData {
+    // 请求列表数据
+}
+
+- (void)setUpUI {
+    // 创建子view并布局
 }
 
 
